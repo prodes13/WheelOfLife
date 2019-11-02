ee6852441ddf3a73ff64b8e2daab091365897925
prodes13 <ciobotaru.florin@gmail.com>
2019-11-02 15:17:44 +0200

    changed chart to polar, changed json to js for import

--- frontend/public/src/app.js
+++ frontend/public/src/app.js
@@ -1,6 +1,9 @@
 import {getChart} from './chart.js';
 import {datasetsLabels, datasetsValues} from './chartDimensions.js';
+import data from '../data/data.js';
 
 var ctx = document.getElementById("myChart");
+
+console.log(data.query);
 
 var resultsChart = getChart(ctx, datasetsLabels, datasetsValues);
--- frontend/public/src/chart.js
+++ frontend/public/src/chart.js
@@ -1,17 +1,17 @@
 function getChart(ctx, datasetsLabels, datasetsValues) {
     return new Chart(ctx, {
-        type: 'radar',
+        type: 'polarArea',
         data: {
             labels: datasetsLabels,
             datasets: datasetsValues
     
         },
         options: {
-            elements: {
-                point: {
-                    radius: 0
-                }
-            },
+            // elements: {
+            //     point: {
+            //         radius: 0
+            //     }
+            // },
             scale: {
                 pointLabels: {
                     fontSize: 12
@@ -25,47 +25,10 @@ function getChart(ctx, datasetsLabels, datasetsValues) {
                 }
             }, 
             legend: {
-                display: false
+                display: true
             }
         }
     }); 
 }
 
 export {getChart};
-
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
