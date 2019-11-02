d4cecef3513e57a0c189fc46cc448c66c4bcf637
prodes13 <ciobotaru.florin@gmail.com>
2019-11-02 11:03:46 +0200

    refractored app.js, added some colors

--- frontend/public/src/app.js
+++ frontend/public/src/app.js
@@ -1,81 +1,8 @@
+import {getChart} from './chart.js';
+
 var ctx = document.getElementById("myChart");
+
 var datasetsLabels = ['Health', 'Relationships', 'Environment', 'Career', 'Money', 'Personal growth', 'Brightness of Life', 'Spiritual Life'];
-console.log(datasetsLabels.length);
+var datasetsValues = [7, 8, 4, 2, 8, 5 ,6, 3];
 
-var myRadarChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-        labels: datasetsLabels,
-        datasets: [{
-            label: 'Your results',
-            data: [10, 10, 4, 2, 8, 5 ,6, 3]
-        }],
-                    backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-    },
-    options: {
-        scale: {
-            angleLines: {
-                display: true
-            },
-            ticks: {
-                suggestedMin: 0,
-                suggestedMax: 10
-            }
-        }
-    }
-});
-
-
-
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
+var resultsChart = getChart(ctx, datasetsLabels, datasetsValues)
