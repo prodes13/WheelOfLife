bad0ea12ad218e9cc6c710aa3bd532e2543fb297
prodes13 <ciobotaru.florin@gmail.com>
2019-11-02 11:48:33 +0200

    starting json file example

--- frontend/public/src/app.js
+++ frontend/public/src/app.js
@@ -3,6 +3,6 @@ import {getChart} from './chart.js';
 var ctx = document.getElementById("myChart");
 
 var datasetsLabels = ['Health', 'Relationships', 'Environment', 'Career', 'Money', 'Personal growth', 'Brightness of Life', 'Spiritual Life'];
-var datasetsValues = [7, 8, 4, 2, 8, 5 ,6, 3];
+var datasetsValues = [7, 8.5, 4.3, 2, 8, 5 ,6, 3];
 
-var resultsChart = getChart(ctx, datasetsLabels, datasetsValues)
+var resultsChart = getChart(ctx, datasetsLabels, datasetsValues);
