307cf48f036f87f895ae33ebf2575e456b78f691
prodes13 <ciobotaru.florin@gmail.com>
2019-11-03 17:45:59 +0200

    layout for questions added

--- frontend/public/src/app.js
+++ frontend/public/src/app.js
@@ -2,7 +2,7 @@ import {getChart} from './chart.js';
 import {datasetsLabels, setValues} from './chartDimensions.js';
 import {showQuestions} from './showQuestions.js';
 
-var done = true;
+var done = false;
 
 // canvas to draw
 var ctx = document.getElementById("myChart");
@@ -10,7 +10,7 @@ var ctx = document.getElementById("myChart");
 // fetching data from a fake server
 fetch('../data/data.json')
     .then(response => response.json())
-    .then(data => showQuestions(data.query, 3));
+    .then(data => showQuestions(data.query));
 
 // setting values for different fields
 var datasetsValues = setValues([4,6,3,4,5,6,7,5]);
@@ -19,3 +19,4 @@ var datasetsValues = setValues([4,6,3,4,5,6,7,5]);
 if(done) {
     var resultsChart = getChart(ctx, datasetsLabels, datasetsValues);
 }
+
--- frontend/public/src/showQuestions.js
+++ frontend/public/src/showQuestions.js
@@ -1,15 +1,52 @@
-var questionPlaceholder = document.getElementById('question');
-var answers = [];
-for(var i = 0; i < 5; i++) {
-    answers[i] = document.getElementById('answer'+(i+1));
-}
+var quizContainer = document.getElementById('quiz');
+var question = document.getElementById('question');
+var answers = document.getElementById('answers');
+var currentQuestion = 0;
 
-export function showQuestions(data, num) {
-    questionPlaceholder.innerText = data[0].questions[num].question;
-    var answersText = data[0].questions[num].answers;
-    console.log(Object.values(answersText));
-    for(var i = 0; i<answers.length; i++ ) {
-        answers[i].innerText = Object.values(answersText)[i];
-    }
-    
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+
+
+function buildQuiz(myQuestions, num) {
+    // for each question...
+    question.innerHTML = myQuestions[num].question;
+    answers.innerHTML = '';
+    for (var i in Object.values(myQuestions[num].answers)) {
+            let choice = Object.values(myQuestions[num].answers)[i];
+            answers.innerHTML += `<label>
+            <input type="radio" name="question${num}" value="${choice}">
+                ${choice}
+            </label>`;        
+        }
+}
+  
+export function showQuestions(data) {
+    // quizContainer.innerText = data[0].questions[0].question;
+    // var answersText = data[0].questions[0].answers;
+    buildQuiz(data[1].questions, currentQuestion);
+   console.log(currentQuestion);
+  
+    // showSlide(0);
+  
+    // on submit, show results
+    // submitButton.addEventListener("click", showResults);
+
+    previousButton.addEventListener("click", function() {
+        console.log("nr intreb ", currentQuestion);
+        console.log("<>", data[1].questions.length);
+        if(currentQuestion >= 1) {
+            currentQuestion--;
+            buildQuiz(data[1].questions, currentQuestion); 
+            };
+        }
+      );
+    nextButton.addEventListener("click", function() {
+        console.log("nr intreb ", currentQuestion);
+        console.log("<>", data[1].questions.length);
+        
+        if(currentQuestion < data[1].questions.length-1) {
+            currentQuestion++;
+            buildQuiz(data[1].questions, currentQuestion); 
+            };
+        });
 }
